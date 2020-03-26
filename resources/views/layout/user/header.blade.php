b48aab0d6be6e0316c9ba2f7a5cfd16c00c0b703
Buffalo <daoleduyhung04@gmail.com>
2020-03-26 11:07:07 +0700

    Update header.blade.php

--- resources/views/layout/user/header.blade.php
+++ resources/views/layout/user/header.blade.php
@@ -1,7 +1,3 @@
-<html xmlns="http://www.w3.org/1999/xhtml"><head><title>
-	Cổng thông tin điện tử: Khoa CNTT Trường Đại học Thông tin Liên Lạc
-</title><meta name="description" content="Cổng thông tin điện tử, trường đại học thông tin, đại học thông tin liên lạc,tuyển sinh đại học quân sự dân sự, sĩ quan thông tin. Update tin tức giáo dục đào tạo 24/24. Thông tin liên quan đên quân sự, công nghệ thông tin, điện tử truyền thông chỉ có tại cổng thông tin đại học thông tin liên lạc."><meta name="keywords" itemprop="keywords" content="si quan thong tin, sĩ quan thông tin, dai hoc thong tin, đại học thông tin, đai hoc thong tin lien lac, đại học thông tin liên lạc, tuyen sinh quan su, tuyển sinh quân sự, dai hoc quan su, đại học quân sự, tuyen sinh CNTT, tuyển sinh CNTT, dien tu vien thong, điện tử viễn thông, dien tu truyen thong, điện tử truyền thông, Đại học thông tin liên lạc, truong si quan thong tin"><link rel="canonical" href="http://www.tcu.edu.vn/"><link rel="alternate" media="handheld" href="http://www.tsqtt.edu.vn/"><meta name="robots" content="index,follow,noodp"><meta property="og:site_name" content="Cổng thông tin đại học thông tin liên lạc"><meta property="og:type" content="website"><meta property="og:title" name="title" content="Đại học thông tin liên lạc | đại học thông tin | sĩ quan thông tin"><meta property="og:url" itemprop="url" content="http://www.tcu.edu.vn/"><meta name="copyright" content="Copyright © 2014 by TCU.EDU.VN">
-
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta name="description" content="">
@@ -10,26 +6,13 @@
 <base href="{{asset('')}}">
 <title></title>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css">
+{{-- <link rel="stylesheet" type="text/css" href="bootstrap/css/home.css"> --}}
+
+<!-- Bootstrap core CSS -->
+<link href="bootstrap/startbootstrap-blog-post-gh-pages/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="bootstrap/startbootstrap-blog-post-gh-pages/css/blog-post.css" rel="stylesheet">
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <script src="ckeditor/ckeditor.js"></script>
-
-<script type="text/javascript" src="/Sites/www/Theme/Script/jquery-1.9.1.js"></script><style></style>
-<script type="text/javascript" src="/Sites/www/Theme/Script/jquery-migrate-1.1.0.js"></script>
-<script type="text/javascript" src="/Sites/www/Theme/Script/script.js"></script>
-
-<link rel="stylesheet" type="text/css" href="/Sites/www/Theme/Style/style.css">
-
-<script type="text/javascript" src="/Sites/www/Theme/Script/jquery.carouFredSel.js"></script>
-<link rel="stylesheet" type="text/css" href="/Sites/www/Theme/Style/caro.css">
-
-<script type="text/javascript" src="/Sites/www/Theme/Script/jquery.fancybox.js?v=2.1.5"></script>
-<link href="/Sites/www/Theme/Style/jquery.fancybox.css?v=2.1.5" rel="stylesheet" type="text/css" media="screen">
-
-<script type="text/javascript" src="/Sites/www/Theme/Script/jquery.lightbox-0.5.js"></script>
-<link rel="stylesheet" type="text/css" href="/Sites/www/Theme/Style/jquery.lightbox-0.5.css">
-
-<script type="text/javascript" src="/Sites/www/Theme/Script/jquery.easing.min.js"></script>
-
-<script type="text/javascript" src="/Sites/www/Theme/Script/jquery.cookie.js"></script>
-</head>
