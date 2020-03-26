d6da8844ebf6837bf5ba3dce6766a6733be5c529
Buffalo <daoleduyhung04@gmail.com>
2020-03-26 11:07:10 +0700

    Update menu.blade.php

--- resources/views/layout/user/menu.blade.php
+++ resources/views/layout/user/menu.blade.php
@@ -1,131 +1,60 @@
-<div id="navigation">
-  <div id="topMenu">
-      <ul class="dropdown">
-          <li>
-              <a class="current" href="/">
-                  <div class="homeicon"></div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+        <a href="{{ route('home') }}"><img class="logo" src="/bootstrap/img/LogoTruong.png" /></a>
+        <a class="navbar-brand" href="{{ route('home') }}" title="Trang chủ">ĐHTTLL</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+            <a class="nav-link" href="
+            {{-- {{route('dsdetai')}} --}}
+            ">Giới thiệu</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="">Tin tức - Sự kiện</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="">Bộ môn</a>
+                <ul class="navbar-item">    
+                    <li class="nav-item active">
+                        <a class="nav-link" href="">Phần mềm</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="">Mạng</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="">Học liệu điện tử</a>
+            </li>
+            @if(Auth::check())
+            @if(Auth::user()->level==1)
+              <li class="nav-item active">
+                <a class="nav-link" href="
+                {{ route('getduyetdt') }}
+                " >Duyệt đề tài
+                </a>
+              </li>
+              <li class="nav-item active">
+                  <a class="nav-link" href="{{ route('getaddadmin') }}">Thêm admin</a>
+              </li>
+            @endif
+            <li class="nav-item ">
+              <a class="nav-link" href="{{ route('logout') }}">Đăng xuất</a>
+            </li>
+          @else 
+            <li class="nav-item active">
+              <a class="nav-link" href="{{ route('getlogin') }}" >Đăng nhập
               </a>
-          </li>
-          
-                  <li>
-                      <a id="ctl00_ctl13_cRptList_cHplName_0" class="current" topmenu="true" exe="Text:Menu.Name,NavigateUrl:Menu.Url">Giới thiệu</a>
-                      
-                              <ul style="left: auto; margin-left: -12px; display: none; width: auto;">
-                                  <abc style="padding: 9px 30px 15px 12px; margin-left: 0px;">
-                          
-                              <li style="float: none; width: auto; display: block; padding: 1px 0px;">
-                                  <div class="sub">
-                                      <a id="ctl00_ctl13_cRptList_cRptList_0_cHplName_0" submenu="true" exe="Text:Menu.Name,NavigateUrl:Menu.Url" href="/Page218/Gioi-thieu-nha-truong.aspx?ID=507">Giới thiệu Nhà trường</a>
-                                  </div>
-                              </li>
-                          
-                              <li style="float: none; width: auto; display: block; padding: 1px 0px;">
-                                  <div class="sub">
-                                      <a id="ctl00_ctl13_cRptList_cRptList_0_cHplName_1" submenu="true" exe="Text:Menu.Name,NavigateUrl:Menu.Url" href="/Page218/Su-menh-tam-nhin.aspx?ID=506">Sứ mệnh - Tầm nhìn</a>
-                                  </div>
-                              </li>
-                          
-                              <li style="float: none; width: auto; display: block; padding: 1px 0px;">
-                                  <div class="sub">
-                                      <a id="ctl00_ctl13_cRptList_cRptList_0_cHplName_2" submenu="true" exe="Text:Menu.Name,NavigateUrl:Menu.Url" href="/Page218/Co-cau-to-chuc.aspx?ID=508">Cơ cấu Tổ chức</a>
-                                  </div>
-                              </li>
-                          
-                              </abc>
-                          </ul>
-                          
-                  </li>
-              
-                  <li>
-                      <a id="ctl00_ctl13_cRptList_cHplName_1" class="current" topmenu="true" exe="Text:Menu.Name,NavigateUrl:Menu.Url" href="/Page217/Tin-tuc-su-kien.aspx?ID=97">Tin tức - Sự kiện</a>
-                      
-                  </li>
-              
-                  <li>
-                      <a id="ctl00_ctl13_cRptList_cHplName_2" class="current" topmenu="true" exe="Text:Menu.Name,NavigateUrl:Menu.Url" href="/Page4473/Thong-Tin-Tuyen-Sinh.aspx?ID=99">Đào tạo</a>
-                      
-                  </li>
-              
-                  <li>
-                      <a id="ctl00_ctl13_cRptList_cHplName_3" class="current" topmenu="true" exe="Text:Menu.Name,NavigateUrl:Menu.Url">Khoa học công nghệ</a>
-                      
-                              <ul style="left: auto; margin-left: -12px; display: none; width: auto;">
-                                  <abc style="padding: 9px 30px 15px 12px; margin-left: 0px;">
-                          
-                              <li style="float: none; width: auto; display: block; padding: 1px 0px;">
-                                  <div class="sub">
-                                      <a id="ctl00_ctl13_cRptList_cRptList_3_cHplName_0" submenu="true" exe="Text:Menu.Name,NavigateUrl:Menu.Url" href="/Page217/Thong-tin-KHCN.aspx?ID=101">Thông tin KHCN</a>
-                                  </div>
-                              </li>
-                          
-                              <li style="float: none; width: auto; display: block; padding: 1px 0px;">
-                                  <div class="sub">
-                                      <a id="ctl00_ctl13_cRptList_cRptList_3_cHplName_1" submenu="true" exe="Text:Menu.Name,NavigateUrl:Menu.Url" href="/Page218/San-pham-KHCN.aspx?ID=136">Sản phẩm KHCN</a>
-                                  </div>
-                              </li>
-                          
-                              </abc>
-                          </ul>
-                          
-                  </li>
-              
-                  <li>
-                      <a id="ctl00_ctl13_cRptList_cHplName_4" class="current" topmenu="true" exe="Text:Menu.Name,NavigateUrl:Menu.Url" href="/Page1395/CLB-Sinh-vien.aspx?ID=111">Giáo dục QPAN</a>
-                      
-                  </li>
-              
-                  <li>
-                      <a id="ctl00_ctl13_cRptList_cHplName_5" class="current" topmenu="true" exe="Text:Menu.Name,NavigateUrl:Menu.Url" href="/Page4473/Thong-Tin-Tuyen-Sinh.aspx?ID=99">Thông tin tuyển sinh</a>
-                      
-                  </li>
-              
-                  <li>
-                      <a id="ctl00_ctl13_cRptList_cHplName_6" class="current" topmenu="true" exe="Text:Menu.Name,NavigateUrl:Menu.Url">Học liệu điện tử</a>
-                      
-                              <ul style="left: auto; margin-left: -12px; display: none; width: auto;">
-                                  <abc style="padding: 9px 30px 15px 12px; margin-left: 0px;">
-                          
-                              <li style="float: none; width: auto; display: block; padding: 1px 0px;">
-                                  <div class="sub">
-                                      <a id="ctl00_ctl13_cRptList_cRptList_6_cHplName_0" submenu="true" exe="Text:Menu.Name,NavigateUrl:Menu.Url" href="/Page4545/Bai-Giang-Tu-Hoc.aspx">Bài giảng điện tử</a>
-                                  </div>
-                              </li>
-                          
-                              <li style="float: none; width: auto; display: block; padding: 1px 0px;">
-                                  <div class="sub">
-                                      <a id="ctl00_ctl13_cRptList_cRptList_6_cHplName_1" submenu="true" exe="Text:Menu.Name,NavigateUrl:Menu.Url" href="/Page4517/Bai-Giang-Tu-Hoc.aspx">Bài giảng ĐT tự học</a>
-                                  </div>
-                              </li>
-                          
-                              <li style="float: none; width: auto; display: block; padding: 1px 0px;">
-                                  <div class="sub">
-                                      <a id="ctl00_ctl13_cRptList_cRptList_6_cHplName_2" submenu="true" exe="Text:Menu.Name,NavigateUrl:Menu.Url">Giáo trình điện tử</a>
-                                  </div>
-                              </li>
-                          
-                              <li style="float: none; width: auto; display: block; padding: 1px 0px;">
-                                  <div class="sub">
-                                      <a id="ctl00_ctl13_cRptList_cRptList_6_cHplName_3" submenu="true" exe="Text:Menu.Name,NavigateUrl:Menu.Url">Thư viện số</a>
-                                  </div>
-                              </li>
-                          
-                              <li style="float: none; width: auto; display: block; padding: 1px 0px;">
-                                  <div class="sub">
-                                      <a id="ctl00_ctl13_cRptList_cRptList_6_cHplName_4" submenu="true" exe="Text:Menu.Name,NavigateUrl:Menu.Url" href="http://elearning.mitechcenter.vn/" target="_blank">Elearning</a>
-                                  </div>
-                              </li>
-                          
-                              </abc>
-                          </ul>
-                          
-                  </li>
-              
-                  <li>
-                      <a id="ctl00_ctl13_cRptList_cHplName_7" class="current" topmenu="true" exe="Text:Menu.Name,NavigateUrl:Menu.Url" href="/Page3462/Thong-tin-hoi-dap.aspx">Liên hệ</a>
-                      
-                  </li>
-              
-      </ul>
-      <div class="clear"></div>
-  </div>
-</div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('getregister') }}">Đăng ký</a>
+            </li>
+          @endif
+        </ul>
+      </div>
+    </div>
+  </nav>
+  
